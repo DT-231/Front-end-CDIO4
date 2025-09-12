57481399d7ec8bd409eb7e3266844964aa1e8288
hoaianh204 <hoaianhh204@gmail.com>
2025-09-12 10:54:23 +0700

    loginpage

---
+++ js/login.js
@@ -0,0 +1,25 @@
+const signUpButton = document.getElementById('signUp');
+const signInButton = document.getElementById('signIn');
+const container = document.getElementById('container');
+
+signUpButton.addEventListener('click', () => {
+	container.classList.add("right-panel-active");
+});
+
+signInButton.addEventListener('click', () => {
+	container.classList.remove("right-panel-active");
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+    const userBtn = document.getElementById("userBtn");
+    const userDropdown = document.getElementById("userDropdown");
+
+    userBtn.addEventListener("click", function (e) {
+        e.stopPropagation();
+        userDropdown.style.display = userDropdown.style.display === "block" ? "none" : "block";
+    });
+
+    document.addEventListener("click", function () {
+        userDropdown.style.display = "none";
+    });
+});
